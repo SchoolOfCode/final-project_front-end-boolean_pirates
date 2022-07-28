da94d112a98c16a4fafcfd7c352cf058de77c31b
AfamOkoh <afamokoh@gmail.com>
2022-07-28 14:17:01 +0100

    style: change header style

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,13 @@
-import Layout from '../components/layout'
-import { useFetchUser } from '../lib/user'
+// import Head from 'next/head';
+// import Image from 'next/image';
 
-const Home = () => {
-  const { user, loading } = useFetchUser()
+import Header from '../components/header';
 
+export default function Home() {
   return (
-    <Layout user={user} loading={loading}>
-      <h1 className="text-red-400">Next.js and Auth0 Example</h1>
-
-      {loading && <p>Loading login info...</p>}
-
-      {!loading && !user && (
-        <>
-          <p>
-            To test the login click in <i>Login</i>
-          </p>
-          <p>
-            Once you have logged in you should be able to click in{' '}
-            <i>Profile</i> and <i>Logout</i>
-          </p>
-        </>
-      )}
-
-      {user && (
-        <>
-          <h4>Rendered user info on the client</h4>
-          <img src={user.picture} alt="user picture" />
-          <p>nickname: {user.nickname}</p>
-          <p>name: {user.name}</p>
-        </>
-      )}
-    </Layout>
-  )
+    <>
+      <Header />
+      <div className='bg-black w-full h-5'></div>;
+    </>
+  );
 }
-
-export default Home
