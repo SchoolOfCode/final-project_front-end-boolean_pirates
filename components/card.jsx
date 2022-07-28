dad096c441c1e74ea29f003a2548792b5a3b633e
HairiBeast <shpetzla@gmail.com>
2022-07-28 12:02:45 +0100

    feature: Added placeholder image to homepage and created a tutorial card

--- components/card.jsx
+++ components/card.jsx
@@ -8,7 +8,6 @@ const Card = ({
 }) => {
   return (
     <div>
-      <h1 className="text-red-400">Next.js and Auth0 Example</h1>
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <img className="w-full" src={imageURL} alt={title} />
         <div className="px-6 py-4">
@@ -18,12 +17,6 @@ const Card = ({
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             {hashtag1}
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            {hashtag2}
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            {hashtag3}
           </span>
         </div>
       </div>
